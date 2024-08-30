d2c0247bb4c3da4f6f0c6b9338448e71bdf0f7ad
Chirandip Roy <chirandiproy@gmail.com>
2024-08-30 20:38:09 +0530

    Update Intermediate P.py

--- Intermediate P.py
+++ Intermediate P.py
@@ -1,23 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL of the website to scrape
-url = 'https://www.shadowfox.in/#'
-
-# Send a request to the website
+url = 'https://www.shadowfox.in/'
 response = requests.get(url)
 
-# Check if the request was successful
 if response.status_code == 200:
-    # Parse the HTML content
     soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Find all article titles (assuming they are in <h2> tags)
     articles = soup.find_all('p')
-
-    # Print the titles of the articles
     for idx, article in enumerate(articles):
-        print(f"{idx + 1}. {article.get_text()}")
+        print(idx + 1,article.get_text())
 else:
     print("Failed to retrieve the webpage.")
 
